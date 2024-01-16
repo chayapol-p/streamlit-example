4f351a89d997754258206be2c414c1df1dcd2431
Chayapol Parisonyu <84707842+chayapol-p@users.noreply.github.com>
2024-01-16 14:07:15 +0000

    feat: add login and access

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,88 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+from streamlit_option_menu import option_menu
 
-from landing import landing
-from data_editor import data_editor
-from my_dashboard import simple_dashboard
+import yaml
+from yaml.loader import SafeLoader
+
+from views.landing import landing
+from views.data_editor import data_editor
+from views.my_dashboard import simple_dashboard
+from views.data_reviewer import data_reviewer
+
+
+
+st.set_page_config(layout="wide")
 
 page_names_to_funcs = {
-    "—": landing,
-    "data_editor": data_editor,
-    "simple_dashboard": simple_dashboard
+    "Home": landing,
+    "Data Editor": data_editor,
+    "Simple Dashboard": simple_dashboard
 }
 
 
-demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
-page_names_to_funcs[demo_name]()
+with open('./credentials/authentication.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+
+
+
+
+# expander = st.expander("Choose a demo")
+# demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
+# page_names_to_funcs[demo_name]()
+
+
+
+authenticator.login('Login', 'main')
+
+if st.session_state["authentication_status"]:
+
+    if st.session_state["name"] == "Admin":
+        page_names_to_funcs = {
+            "Home": landing,
+            "Data Editor": data_editor,
+            "Simple Dashboard": simple_dashboard,
+            "Data Review": data_reviewer
+        }
+    else:
+        page_names_to_funcs = {
+            "Home": landing,
+            "Data Editor": data_editor,
+            "Simple Dashboard": simple_dashboard
+        }
+    
+    with st.sidebar:
+
+        st.header("Choose a demo")
+
+        demo_name = option_menu(
+            menu_title=None,  # required
+            options=list(page_names_to_funcs.keys()),  # required
+            icons=["house", "table", "graph-up"],  # optional
+            menu_icon="menu-down",
+            default_index=0,  # optional
+            styles={
+                "container": {"padding": "0!important", "background-color": "#fafafa"},
+                "icon": {"color": "orange", "font-size": "25px"}, 
+                "nav-link": {"font-size": "14px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+                "nav-link-selected": {"background-color": "green"},
+            }
+        )
+
+    authenticator.logout('Logout', 'sidebar')
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    page_names_to_funcs[demo_name]()
+
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
